76aa6af75f2e979e29e8db5cdcf32f3c5140ed68
Moustapha yaya sow <Sowmoustaphayaya@gmail.com>
2024-07-03 15:56:20 +0200

    Push origin master

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const productRoute = require("./route/product.route");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+app.use('/product', productRoute);
+
+module.exports = app;
